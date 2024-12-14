a6ea55ca242ba805547f2b9450f2a76bc17784ab
andreykovalev <andreyykovalev@gmail.com>
2024-12-14 11:59:50 +0100

    quick sort

--- src/main/java/org/algo/Trees.java
+++ src/main/java/org/algo/Trees.java
@@ -241,7 +241,7 @@ public class Trees {
         merge(nums, leftHalfArray, rightHalfArray);
     }
 
-    private static void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
+    private void merge(int[] inputArray, int[] leftHalf, int[] rightHalf) {
         int leftSize = leftHalf.length;
         int rightSize = rightHalf.length;
 
@@ -269,5 +269,59 @@ public class Trees {
             k++;
             j++;
         }
+    }
+
+    private void quicksort(int[] array, int lowIndex, int highIndex) {
+
+        if (lowIndex >= highIndex) {
+            return;
+        }
+
+        int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, highIndex);
+
+        int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
+        quicksort(array, lowIndex, leftPointer - 1);
+        quicksort(array, leftPointer + 1, highIndex);
+
+    }
+
+    private int partition(int[] array, int lowIndex, int highIndex, int pivot) {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex - 1;
+
+        while (leftPointer < rightPointer) {
+
+            // Walk from the left until we find a number greater than the pivot, or hit the right pointer.
+            while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+
+            // Walk from the right until we find a number less than the pivot, or hit the left pointer.
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(array, leftPointer, rightPointer);
+        }
+
+        // This is different from what the video has, and fixes an issue where the last value could potentially be out of order.
+        // Thanks to viewer Wilson Barbosa for suggesting the fix!
+        if(array[leftPointer] > array[highIndex]) {
+            swap(array, leftPointer, highIndex);
+        }
+        else {
+            leftPointer = highIndex;
+        }
+
+        return leftPointer;
+    }
+
+    private void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
