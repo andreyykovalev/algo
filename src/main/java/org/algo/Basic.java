799b0462d0187ca77d8a9d0dd04292fc505c2e93
andreykovalev <andreyykovalev@gmail.com>
2026-04-28 15:40:01 +0200

    running sum

--- src/main/java/org/algo/Basic.java
+++ src/main/java/org/algo/Basic.java
@@ -6,6 +6,19 @@ import java.util.stream.Stream;
 
 public class Basic {
 
+    public int[] runningSum(int[] nums) {
+
+        int[] output = new int[nums.length];
+        int currentSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+
+            output[i] = currentSum + nums[i];
+            currentSum += nums[i];
+        }
+
+        return output;
+    }
+
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
